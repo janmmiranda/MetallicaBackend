79e905e13a15b942f10d23a284d7b43639cd0947
janmiran <janmmiranda@gmail.com>
2018-09-28 13:26:53 +0530

    Initial commit of project

---
+++ src/main/java/com/sapient/metallica/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sapient.metallica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MetallicaApplication.class);
+	}
+
+}

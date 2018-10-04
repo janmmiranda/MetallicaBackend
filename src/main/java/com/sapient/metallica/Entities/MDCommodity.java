d27934620e0c6d3d1e9ca6cb16a91f28dba81b44
janmiran <janmmiranda@gmail.com>
2018-10-04 16:04:16 +0530

    Created MDCommodity entity, Notification package and class, added all delete and update for Trade Controller and commented out certain attributes from TradeEntity

---
+++ src/main/java/com/sapient/metallica/Entities/MDCommodity.java
@@ -0,0 +1,54 @@
+package com.sapient.metallica.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name="MarketData")
+@XmlRootElement
+public class MDCommodity {
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int mdId;
+	
+//	@ManyToOne
+//	RefDataCommodity commodity;
+	
+	float price;
+
+//	public RefDataCommodity getCommodity() {
+//		return commodity;
+//	}
+//
+//	public void setCommodity(RefDataCommodity commodity) {
+//		this.commodity = commodity;
+//	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getMdId() {
+		return mdId;
+	}
+	
+	public MDCommodity() {
+		super();
+	}
+	
+//	public MDCommodity(RefDataCommodity commodity, float price) {
+//		this.commodity = commodity;
+//		this.price = price;
+//	}
+	
+}
--- src/main/java/com/sapient/metallica/Repos/RefDataRepo.java
+++ src/main/java/com/sapient/metallica/Repos/RefDataRepo.java
@@ -3,6 +3,9 @@ package com.sapient.metallica.Repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sapient.metallica.Entities.RefDataCounterParty;
 
-public interface RefDataRepo extends JpaRepository<RefDataCounterParty, Integer>{
+//public interface RefDataRepo extends JpaRepository<RefDataCounterParty, Integer>{
+//	
+//}
+public interface RefDataRepo {
 	
 }

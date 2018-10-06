726dd9cc95fdbdbcc79cec0cac8b6afeb9302a9d
abhinish kumar <abhinishkumar5488@gmail.com>
2018-10-06 11:55:30 +0530

    market data

--- src/main/java/com/sapient/metallica/Entities/MDCommodity.java
+++ src/main/java/com/sapient/metallica/Entities/MDCommodity.java
@@ -4,7 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -17,19 +18,34 @@ public class MDCommodity {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	int mdId;
 	
+	float price=getRandomNumber();// random price
+	String symbol;
+	String name;
+	
+//	@Autowired
+//	RefDataCommodityRepo jpaCMD;
+//	
+//	@RequestMapping(path="/allproduct", method=RequestMethod.GET)
+//	public List<RefDataCommodity> findAllCMD(){
+//		List<RefDataCommodity> List1 = jpaCMD.findAll();
+//				return List1;
+//	}
+	
+
+	
 //	@ManyToOne
 //	RefDataCommodity commodity;
 	
-	float price;
-
-//	public RefDataCommodity getCommodity() {
-//		return commodity;
-//	}
-//
-//	public void setCommodity(RefDataCommodity commodity) {
-//		this.commodity = commodity;
-//	}
-
+	public MDCommodity() {
+		super();
+	}
+	
+	public MDCommodity(String symbol, String name) {
+		super();
+		this.symbol = symbol;
+		this.name = name;
+	}
+	
 	public float getPrice() {
 		return price;
 	}
@@ -41,10 +57,47 @@ public class MDCommodity {
 	public int getMdId() {
 		return mdId;
 	}
-	
-	public MDCommodity() {
-		super();
+
+
+	public static float getRandomNumber(){
+	    float x = (float)(Math.random() * 5000 + 500);//price range between 500 to 5000
+	    return x;
 	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return "MDCommodity [mdId=" + mdId + ", price=" + price + ", symbol=" + symbol + ", name=" + name + "]";
+	}
+	
+	 
+//	public RefDataCommodity getCommodity() {
+//		return commodity;
+//	}
+//
+//	public void setCommodity(RefDataCommodity commodity) {
+//		this.commodity = commodity;
+//	}
+
+	
+	
 	
 //	public MDCommodity(RefDataCommodity commodity, float price) {
 //		this.commodity = commodity;
